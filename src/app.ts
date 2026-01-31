d332ed8cd1e38130eee576f5bb44a3bdfa697c24
remi <remi@remi>
2026-01-31 15:01:27 +0100

    first

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ import messageRoutes from "./routes/message.routes";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
